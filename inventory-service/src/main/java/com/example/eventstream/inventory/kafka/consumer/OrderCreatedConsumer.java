f09d6b881536aa3a579370959be141685c71136a
Anuj Yadav <106235866+anujyadav11@users.noreply.github.com>
2026-06-29 12:11:47 +0530

    Create Inventory Service and consume OrderCreatedEvent

---
+++ inventory-service/src/main/java/com/example/eventstream/inventory/kafka/consumer/OrderCreatedConsumer.java
@@ -0,0 +1,22 @@
+package com.example.eventstream.inventory.kafka.consumer;
+
+import com.example.eventstream.inventory.kafka.event.OrderCreatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderCreatedConsumer {
+    private static final Logger log = LoggerFactory.getLogger(OrderCreatedConsumer.class);
+
+    @KafkaListener(
+            topics = "order-created",
+            groupId = "inventory-group"
+    )
+    public void consume(OrderCreatedEvent event){
+        log.info("Received OrderCreatedEvent: {}", event);
+        log.info("Reserving inventory for order {}",event.orderId());
+        log.info("Inventroy reserved successfully");
+    }
+}
